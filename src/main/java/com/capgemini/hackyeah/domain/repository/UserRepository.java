bc5be6cd09e85513d498367a5c5308677fdeaa15
isishchu <55sumbuL>
2023-09-30 19:42:24 +0200

    fixed optional problem

--- src/main/java/com/capgemini/hackyeah/domain/repository/UserRepository.java
+++ src/main/java/com/capgemini/hackyeah/domain/repository/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    Optional<User> findFirstByEmail(String email);
     Optional<User> findByEmail(String email);
 
 }
