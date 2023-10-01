d20cb9b08c8f0c1c363ed96fe40a7d2710560206
isishchu <55sumbuL>
2023-10-01 09:16:16 +0200

    cors disable

--- src/main/java/com/capgemini/hackyeah/auth/controller/AuthenticationController.java
+++ src/main/java/com/capgemini/hackyeah/auth/controller/AuthenticationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowCredentials = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
