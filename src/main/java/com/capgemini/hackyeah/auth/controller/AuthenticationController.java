83ebcdd72071e38f578c111b470b8cfd1a9dc7c2
isishchu <55sumbuL>
2023-10-01 08:08:30 +0200

    refactoring

--- src/main/java/com/capgemini/hackyeah/auth/controller/AuthenticationController.java
+++ src/main/java/com/capgemini/hackyeah/auth/controller/AuthenticationController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
